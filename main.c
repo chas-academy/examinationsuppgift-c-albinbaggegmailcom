f7fe14a25e47708e3a1b4efbb5e743b9941e9695
albinbagge@gmail.com <albinbagge@gmail.com>
2026-05-19 22:46:05 +0200

    Clean up code

--- main.c
+++ main.c
@@ -1,27 +1,41 @@
-/*
-      Grundläggande funktionalitet (20p)
-    - 20p Kompilera och köra utan krasch
-
-      Analys: Högst medelpoäng (30p)
-    x 15p Logik - Identifiera vilken elev som har högst medelpoäng
-    x 15p Format - Namnet skrivs ut korrekt med stor begynnelsebokstav
-    
-      Analys: Under gruppens snitt (30p)
-    x 15p Logik - Räkna ut totala medelvärdet, identifiera vilka elever som ligger under medelvärdet
-    x 15p Format - Namnen skrivs ut med stor begynnelsebokstav, skrivs ut i samma ordning som de matades in
-
-    - Video (20p)
-*/
-
 #include <stdio.h>
 #include <ctype.h>
 
+int find_best_student(float student_averages[])
+{
+    float highest_average = 0.0;
+    int best_student;
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (student_averages[i] > highest_average)
+        {
+            highest_average = student_averages[i];
+            best_student = i;
+        }
+    }
+
+    return best_student;
+}
+
+float calculate_total_average(float student_averages[])
+{
+    float added_averages = 0.0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        added_averages += student_averages[i];
+    }
+    
+    return added_averages / 5.0;
+}
+
 int main()
 {
-    // Input data
     char student_names[5][10];
     float student_averages[5];
 
+    // Input data for all students
     for (int i = 0; i < 5; i++)
     {
         // Input student name
@@ -38,50 +52,18 @@ int main()
             total_points += points;
         }
 
+        // Calculate student average points
         student_averages[i] = total_points / 13.0;
-    }
-    // debug
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     printf("Student %d = %s\n", i, student_names[i]);
-    //     printf("student_averages[%d] = %.2f\n", i, student_averages[i]);
-    // }
-
-    // Compare student point averages
-    float highest_average = 0.0;
-    int best_student;
-
-    for (int i = 0; i < 5; i++)
-    {
-        if (student_averages[i] > highest_average)
-        {
-            highest_average = student_averages[i];
-            best_student = i;
-        }
     }
 
     // Print best student
+    int best_student = find_best_student(student_averages);
     printf("%s\n", student_names[best_student]);
-    // debug
-    // printf
-    // (
-    //     "Best: %s, highest_average = %.2f\n", 
-    //     student_names[best_student], highest_average
-    // );
 
-    // Calculate total average of points
-    float added_averages = 0.0;
+    // Determine total average
+    float total_average = calculate_total_average(student_averages);
 
-    for (int i = 0; i < 5; i++)
-    {
-        added_averages += student_averages[i];
-    }
-    
-    float total_average = added_averages / 5.0;
-    // debug
-    // printf("total_average = %.2f\n", total_average);
-
-    // Compare students against total average
+    // Print students under the total average
     for (int i = 0; i < 5; i++)
     {
         if (student_averages[i] < total_average)
